08a1797b6b4cd82851cbdfee3d31663877b645ee
Apisit Angsuphan <60085220+sussus003@users.noreply.github.com>
2025-02-05 17:54:08 +0700

    DOne

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,54 @@
+'use client'
+import React from "react";
+import { useParams } from "next/navigation";
+//MUI
+import Container from "@mui/material/Container";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+interface myParams {
+  params: {
+    id: string;
+  };
+}
+async function getData(id: string) {
+  const res = await fetch(`https://www.melivecode.com/api/attractions/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data from id: " + id);
+  }
+
+  return res.json();
+}
+
+async function page({ params }: myParams) {
+
+  const data = await getData(params.id);
+  console.log(data);
+  return (
+    <Container maxWidth="lg" className="mt-10">
+      <Card >
+        <CardMedia
+          sx={{ height: 140 }}
+          image={data.attraction.coverimage}
+          title={data.attraction.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {data.attraction.name}
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            {data.attraction.detail}
+          </Typography>
+        </CardContent>
+        
+      </Card>
+    </Container>
+  );
+}
+
+export default page;
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='bg-cyan-400 py-4 text-center'>
+        <ul>
+            <li><a href="/" className='text-lg font-bold'>Attractions</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
