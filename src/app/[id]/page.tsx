48efb853f7e7c4cacfb2f3f6486009f613e8acb2
Apisit Angsuphan <60085220+sussus003@users.noreply.github.com>
2025-02-05 18:27:06 +0700

    Done add Netlify.toml

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,14 +1,11 @@
 'use client'
 import React from "react";
-import { useParams } from "next/navigation";
 //MUI
 import Container from "@mui/material/Container";
 
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 interface myParams {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 import * as React from "react";
-import Image from "next/image";
 import Link from "next/link";
 //From MUI
 import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 interface Attraction {
